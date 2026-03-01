528bf3f5ff336dc7910837629d13e31311eee134
SeanGibbons4 <sean.gibbons@kellogg.northwestern.edu>
2026-03-01 23:02:55 +0000

    Step #2 Complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def destroy
+      session["user_id"] = nil
+  end
   
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
-    redirect_to "/login"
+    require "bcrypt"
+
+    user = User.find_by({"email" => params["email"]})
+
+    if user == nil 
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    else
+      if = BCrypt::Password.new(user["password"]) == params["password"]
+        session["user_id"] = user["id"]
+        flash["notice"] = "Sucessful Login"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Incorrect password."
+        redirect_to "/login"
+      
+      end
+    end
   end
 end
