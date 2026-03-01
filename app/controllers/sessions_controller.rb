a9bba4ade04042eb80f3d206f19ab81e192109f6
SeanGibbons4 <sean.gibbons@kellogg.northwestern.edu>
2026-03-01 23:16:09 +0000

    Update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
 
   def destroy
       session["user_id"] = nil
+      flash["notice"] = "Logged Out"
+      redirect_to "/login"
   end
   
   def create
